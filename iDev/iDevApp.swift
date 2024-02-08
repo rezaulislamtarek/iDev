bdd2c39aabba72006d3bb1a01b07026310f8fe19
rezaulislam <mrezaulislam.t@gmail.com>
2024-02-08 19:02:51 +0600

    Initial Commit

---
+++ iDev/iDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDevApp.swift
+//  iDev
+//
+//  Created by Rezaul Islam on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct iDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
